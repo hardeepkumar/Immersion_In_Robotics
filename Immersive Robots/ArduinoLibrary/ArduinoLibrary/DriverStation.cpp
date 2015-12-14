b0a12cb90ea16c00f91780c02360c9d18d3f109d
Michael B <mjbradvica@yahoo.com>
2015-12-13 16:13:07 -0800

    Small Update
    Added some documentation.

---
+++ Immersive Robots/ArduinoLibrary/ArduinoLibrary/DriverStation.cpp
@@ -0,0 +1,2 @@
+#include"DriverStation.h"
+
---
+++ Immersive Robots/ArduinoLibrary/ArduinoLibrary/DriverStation.h
@@ -0,0 +1,18 @@
+#ifndef DRIVERSTATION
+#define DRIVERSTATION
+
+#include<Arduino.h>
+#include<SPI.h>
+#include<WiFi.h>
+#include<WiFiUdp.h>
+
+class DriverStation
+{
+public:
+
+private:
+
+};
+
+#endif DRIVERSTATION
+
