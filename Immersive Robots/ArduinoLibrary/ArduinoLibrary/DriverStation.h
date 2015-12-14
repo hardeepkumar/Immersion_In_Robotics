cf0747e0740a4187a6855ec41da87e448a5ac921
Michael B <mjbradvica@yahoo.com>
2015-12-13 22:29:50 -0800

    Small update
    Header file definitions.

--- Immersive Robots/ArduinoLibrary/ArduinoLibrary/DriverStation.h
+++ Immersive Robots/ArduinoLibrary/ArduinoLibrary/DriverStation.h
@@ -5,12 +5,31 @@
 #include<SPI.h>
 #include<WiFi.h>
 #include<WiFiUdp.h>
+#include<string>
 
 class DriverStation
 {
 public:
 
+	enum MotorTypes { Stepper, Servo, Standard };
+	//Ask if we want to separate classes or keep one for simplicity. 
+
 private:
+	char networkID[];
+	char networkPassword[];
+	int networkStatus = WL_IDLE_STATUS;
+	int portNumber;
+
+	char packetBuffer[255];
+	WiFiUDP udpObject;
+	int packetSize;
+	IPAddress remoteIP;
+	int packetLength;
+
+	std::string inputCommand;
+	std::string motorCommands[10];
+	MotorTypes motors[10];
+
 
 };
 
