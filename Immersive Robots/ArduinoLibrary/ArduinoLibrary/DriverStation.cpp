a8225bb82364f01562a80bc987a36659a0ff174a
Michael B <mjbradvica@yahoo.com>
2015-12-15 23:43:48 -0800

    Library Update
    Initial header and functions added.

--- Immersive Robots/ArduinoLibrary/ArduinoLibrary/DriverStation.cpp
+++ Immersive Robots/ArduinoLibrary/ArduinoLibrary/DriverStation.cpp
@@ -1,2 +1,109 @@
 #include"DriverStation.h"
 
+DriverStation::DriverStation(string ssid, string password, int port, int key) : networkID(ssid), networkPassword(password), portNumber(port), keyIndex(key)
+{
+
+}
+
+string DriverStation::GetNetworkID()
+{
+	return networkID;
+}
+
+void DriverStation::SetNetworkID(string id)
+{
+	networkID = id;
+}
+
+string DriverStation::GetNetworkPassword()
+{
+	return networkPassword;
+}
+
+void DriverStation::SetNetworkPassword(string password)
+{
+	networkPassword = password;
+}
+
+int DriverStation::GetPortNumber()
+{
+	return portNumber;
+}
+
+void DriverStation::SetPortNumber(int portNum)
+{
+	portNumber = portNum;
+}
+
+int DriverStation::GetKeyIndex()
+{
+	return keyIndex;
+}
+
+void DriverStation::SetKeyIndex(int key)
+{
+	keyIndex = key;
+}
+
+string DriverStation::GetWiFiID()
+{
+	return WiFi.SSID();
+}
+
+IPAddress DriverStation::GetIPAddress()
+{
+	IPAddress ip = WiFi.localIP();
+	return ip;
+}
+
+long DriverStation::GetSignalStrength()
+{
+	return WiFi.RSSI();
+}
+
+void DriverStation::Initialize()
+{
+	Serial.begin(9600);
+
+	while (networkStatus != WL_CONNECTED)
+	{
+		Serial.print("Connecting to network...");
+		Serial.print(networkID);
+
+		networkStatus = WiFI.begin(networkID, networkPassword);
+
+		delay(5000);
+	}
+
+	Serial.println("Connecting...");
+	Serial.println(GetWiFiID());
+	Serial.println(GetIPAddress());
+	Serial.println(GetSignalStrength());
+
+	Udp.begin(localport);
+}
+
+void DriverStation::Run()
+{
+	packetSize = Udp.parsePacket();
+	packetLength = Udp.read(packetBuffer, 255);
+
+	if (packetLength > 0)
+	{
+		packetBuffer[packetLength] = 0;
+	}
+
+	inputCommand = packetBuffer;
+
+	for (int i = 0; i < inputCommand.length(); i++)
+	{
+		if (inputCommand[i] != ',')
+		{
+			motorCommands[commandCounter] = motorCommands[commandCounter] + inputCommand[i];
+		}
+		else
+		{
+			commandCounter++;
+		}
+	}
+}
--- Immersive Robots/ArduinoLibrary/ArduinoLibrary/DriverStation.h
+++ Immersive Robots/ArduinoLibrary/ArduinoLibrary/DriverStation.h
@@ -7,31 +7,56 @@
 #include<WiFiUdp.h>
 #include<string>
 
-class DriverStation
-{
-public:
-
-	enum MotorTypes { Stepper, Servo, Standard };
-	//Ask if we want to separate classes or keep one for simplicity. 
-
-private:
-	char networkID[];
-	char networkPassword[];
-	int networkStatus = WL_IDLE_STATUS;
-	int portNumber;
-
-	char packetBuffer[255];
-	WiFiUDP udpObject;
-	int packetSize;
-	IPAddress remoteIP;
-	int packetLength;
-
-	std::string inputCommand;
-	std::string motorCommands[10];
-	MotorTypes motors[10];
+using namespace std;
 
 
-};
+	class DriverStation
+	{
+	public:
+
+		enum MotorTypes { Stepper, Servo, Standard };
+
+		DriverStation(string ssid, string password, int port, int key);
+
+		string GetNetworkID();
+		void SetNetworkID(string id);
+		string GetNetworkPassword();
+		void SetNetworkPassword(string password);
+		int GetPortNumber();
+		void SetPortNumber(int portNum);
+		int GetKeyIndex();
+		void SetKeyIndex(int key);
+
+		string GetWiFiID();
+		IPAddress GetIPAddress();
+		long GetSignalStrength();
+
+		void Initialize();
+		void Run();
+
+		//Ask if we want to separate classes or keep one for simplicity. 
+
+	private:
+		string networkID;
+		string networkPassword;
+		int networkStatus = WL_IDLE_STATUS;
+		int portNumber;
+		int keyIndex;
+
+		char packetBuffer[255];
+		WiFiUDP udpObject;
+		int packetSize;
+		IPAddress remoteIP;
+		int packetLength;
+
+		string inputCommand;
+		string motorCommands[10];
+		int commandCounter;
+		MotorTypes motors[10];
+
+
+	};
+
 
 #endif DRIVERSTATION
 
